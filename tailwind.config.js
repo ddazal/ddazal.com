260548110fe0c29d30849ed0a71e285573a22dce
David Daza <03dazal@gmail.com>
2020-04-23 21:26:27 -0500

    Add tailwind to build process. Change styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'open-sans': ['"Open Sans"', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
